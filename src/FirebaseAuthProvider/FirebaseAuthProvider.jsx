f9c9350c4ad6c0df1051d1cb837d006439981cdf
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-06-10 08:49:18 +0600

    token generate and access - 1

--- src/FirebaseAuthProvider/FirebaseAuthProvider.jsx
+++ src/FirebaseAuthProvider/FirebaseAuthProvider.jsx
@@ -4,6 +4,7 @@ import { createContext, useEffect, useState } from "react";
 import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth/web-extension";
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import auth from "../firebase/firebase.config";
+import useAxiosPublic from './../hooks/useAxiosPublic';
 
 
 export const AuthContext = createContext(null);
@@ -14,8 +15,8 @@ const githubLoginProvider = new GithubAuthProvider();
 
 const FirebaseAuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
 
   //create user
   const createUser = (email, password) => {
@@ -56,12 +57,30 @@ const FirebaseAuthProvider = ({ children }) => {
 
   // observer
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
-      setLoading(false); // Set loading to false regardless of user existence
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+        setUser(currentUser);
+        if (currentUser) {
+            // get token and store client
+            const userInfo = { email: currentUser.email };
+            axiosPublic.post('/jwt', userInfo)
+                .then(res => {
+                    if (res.data.token) {
+                        localStorage.setItem('access-token', res.data.token);
+                        setLoading(false);
+                    }
+                })
+        }
+        else {
+            // TODO: remove token (if token stored in the client side: Local storage, caching, in memory)
+            localStorage.removeItem('access-token');
+            setLoading(false);
+        }
+        
     });
-    return () => unsubscribe();
-  }, []);
+    return () => {
+        return unsubscribe();
+    }
+}, [axiosPublic])
 
   const allValues = {
     signInUser,
